5bbe1410ba741dbcb6911f8c1b1a03808af3caea
Lohit244 <Lohit244@gmail.com>
2021-12-03 10:15:30 +0530

    adds navbar for homepage

--- src/App.js
+++ src/App.js
@@ -1,40 +1,39 @@
-import './App.css';
-import Home from './Components/Home/Home';
-import firebase from 'firebase/compat/app';
-import 'firebase/compat/auth';
-import 'firebase/compat/firestore';
-import { useAuthState } from "react-firebase-hooks/auth"
-import Dashboard from './Components/Dashboard/Dashboard';
+import "./App.css";
+import Home from "./Components/Home/Home";
+import firebase from "firebase/compat/app";
+import "firebase/compat/auth";
+import "firebase/compat/firestore";
+import { useAuthState } from "react-firebase-hooks/auth";
+import Dashboard from "./Components/Dashboard/Dashboard";
 
 firebase.initializeApp({
-  apiKey: `${process.env.REACT_APP_FIREBASE_KEY}`,
-  authDomain: `${process.env.REACT_APP_FIREBASE_AUTH_DOMAIN}`,
-  projectId: `${process.env.REACT_APP_FIREBASE_PROJECT_ID}`,
-  storageBucket: `${process.env.REACT_APP_FIREBASE_STORAGE_BUCKET}`,
-  messagingSenderId: `${process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID}`,
-  appId: `${process.env.REACT_APP_FIREBASE_APP_ID}`
-})
-const auth = firebase.auth()
-
+	apiKey: `${process.env.REACT_APP_FIREBASE_KEY}`,
+	authDomain: `${process.env.REACT_APP_FIREBASE_AUTH_DOMAIN}`,
+	projectId: `${process.env.REACT_APP_FIREBASE_PROJECT_ID}`,
+	storageBucket: `${process.env.REACT_APP_FIREBASE_STORAGE_BUCKET}`,
+	messagingSenderId: `${process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID}`,
+	appId: `${process.env.REACT_APP_FIREBASE_APP_ID}`,
+});
+const auth = firebase.auth();
 
 function App() {
-  const signIn = ()=>{
-    const provider = new firebase.auth.GithubAuthProvider();
-    auth.signInWithPopup(provider)
-  }
-  const signOut = ()=>{
-    auth.signOut()
-  }
-  //eslint-disable-next-line
-  const [user,loading,err] = useAuthState(auth)
-  if(err){
-    console.log(err)
-  }
-  return (
-    <div className="App">
-      {user? <Dashboard func={signOut}/> :<Home func={signIn} />}
-    </div>
-  );
+	const signIn = () => {
+		const provider = new firebase.auth.GithubAuthProvider();
+		auth.signInWithPopup(provider);
+	};
+	const signOut = () => {
+		auth.signOut();
+	};
+	//eslint-disable-next-line
+	const [user, loading, err] = useAuthState(auth);
+	if (err) {
+		console.log(err);
+	}
+	return (
+		<div className="App">
+			{user ? <Dashboard func={signOut} user={user} /> : <Home func={signIn} />}
+		</div>
+	);
 }
 
 export default App;
--- src/Components/Dashboard/Dashboard.js
+++ src/Components/Dashboard/Dashboard.js
@@ -1,17 +1,20 @@
 // import firebase from 'firebase/compat/app';
+import { Search } from "@mui/icons-material";
+import { Avatar, Button } from "@mui/material";
+import "./Dashboard.css"
 // import { useCollectionData } from 'react-firebase-hooks/firestore';
-function Dashboard (props){
-    // const firestore = firebase.firestore()
-    // const messagesRef = firestore.collection("Messages")
-    // const query = messagesRef.orderBy("createdAt").limit(25)
+function Dashboard(props) {
+	// const firestore = firebase.firestore()
+	// const messagesRef = firestore.collection("Messages")
+	// const query = messagesRef.orderBy("createdAt").limit(25)
 
-    // const [Messages] = useCollectionData(query, {idField: 'id'});
-    // {Messages && Messages.map(msg=><p key={msg.id}>{msg.message}</p>)}
-    return(
-        <div className="Dashboard">
-            <button onClick={props.func}>Sign Out</button>
-        </div>
-    )
+	// const [Messages] = useCollectionData(query, {idField: 'id'});
+	// {Messages && Messages.map(msg=><p key={msg.id}>{msg.message}</p>)}
+	return (
+		<div className="Dashboard">
+			<Button onClick={props.func} variant="outlined">Sign Out</Button>
+		</div>
+	);
 }
 
-export default Dashboard
+export default Dashboard;
