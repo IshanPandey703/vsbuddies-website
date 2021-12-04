09975d077ae97e9f3b9a1801b4f8184eab918135
Lohit244 <Lohit244@gmail.com>
2021-12-04 18:26:44 +0530

    working chat

--- src/Components/Chat/CurChat.js
+++ src/Components/Chat/CurChat.js
@@ -1,9 +1,17 @@
 /* eslint-disable no-unused-vars */
+import { Send } from "@mui/icons-material";
+import { Button } from "@mui/material";
 import firebase from "firebase/compat";
 import { useState, useEffect } from "react";
+import MessageCard from "./MessageCard/MessageCard";
+import { useCollectionData } from 'react-firebase-hooks/firestore';
 function CurChat(props) {
     const firestore = firebase.firestore();
     const messagesRef = firestore.collection("Users").doc(props.uid).collection("Messages");
+    const recipientRef = firestore.collection("Users").doc(props.curChat).collection("Messages")
+    const recvdquery = messagesRef.where("author","in",[props.uid, props.curChat])
+    const [rcvd] = useCollectionData(recvdquery)
+    const filteredrcvd = rcvd?rcvd.filter(msg=>((msg.author === props.uid && msg.to===props.curChat)||(msg.to === props.uid && msg.to === props.curChat ))):[]
     const [width, setWidth] = useState(window.innerWidth);
     function handleWindowSizeChange() {
         setWidth(window.innerWidth);
@@ -16,11 +24,38 @@ function CurChat(props) {
     }, []);
     const isMobile = width <= 768;
 
+    const [messageInput, setMessageInput] = useState("")
+
+    const messageInputChange= (e)=>{
+        setMessageInput(e.target.value)
+    }
+    const sendMessage = async (e)=>{
+        e.preventDefault();
+        await messagesRef.add({
+            message: messageInput,
+            createdAt: firebase.firestore.FieldValue.serverTimestamp(),
+            author: props.uid,
+            to: props.curChat
+        })
+        await recipientRef.add({
+            message: messageInput,
+            createdAt: firebase.firestore.FieldValue.serverTimestamp(),
+            author: props.uid,
+            to: props.curChat
+        })
+    }
+
     return (
         <div className="CurChat">
-            my:{props.uid}
-            <br/>
-            friend: {props.curChat}
+            <div className="messages-container">
+            {rcvd?(filteredrcvd.map(msgObject=><MessageCard sr={msgObject.author === props.uid?"sent":"recieved"}>{msgObject.message}</MessageCard>)):""}
+            </div>
+            <form className="message-form" onSubmit={sendMessage}>
+                <input className="message-input" value={messageInput} onChange={messageInputChange} placeholder="Message"/>
+                <Button variant="contained" type="submit">
+                    <Send color="#fff" />
+                </Button>
+            </form>
         </div>
     )
 }
