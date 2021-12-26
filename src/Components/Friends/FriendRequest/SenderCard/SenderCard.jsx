c285c3ce2f96db639420f8a8a2030f5135681557
AnkurPandey07 <mailankur1902@gmail.com>
2021-12-26 19:30:54 +0530

    Resolved Conflicts

--- src/Components/Friends/FriendRequest/SenderCard/SenderCard.jsx
+++ src/Components/Friends/FriendRequest/SenderCard/SenderCard.jsx
@@ -1,14 +1,14 @@
 import { Button, ButtonGroup, Avatar } from "@mui/material";
-import  firebase  from "firebase/compat";
+import firebase from "firebase/compat";
 import DoneIcon from '@mui/icons-material/Done';
 import ClearIcon from '@mui/icons-material/Clear';
 import { useState, useEffect } from "react";
 import "./SenderCard.css";
 
-function SenderCard(props){
+function SenderCard(props) {
 
     const db = firebase.firestore();
-    
+
     // senderDetails contains details of sender of friend request
     // let [senderDetails,setSenderDetalis] = useState({});
 
@@ -31,17 +31,17 @@ function SenderCard(props){
 
         // Add sender's uid in receiver's friend List
         const addSender = await receiverRref.update({
-            friends : firebase.firestore.FieldValue.arrayUnion(sender)
+            friends: firebase.firestore.FieldValue.arrayUnion(sender)
         });
 
         // Add receiver's uid in sender's friend List
         const addReceiver = await senderRref.update({
-            friends : firebase.firestore.FieldValue.arrayUnion(receiver)
+            friends: firebase.firestore.FieldValue.arrayUnion(receiver)
         });
 
         // delete the doc with key as Uid of sender in receiver's Pending Req Collection
         const removeSender = await db.collection("Users").doc(receiver).collection("Pending Requests")
-                            .doc(sender).delete();
+            .doc(sender).delete();
     }
 
     async function reqDecline() {
@@ -50,39 +50,40 @@ function SenderCard(props){
         const receiverRef = db.collection("Users").doc(receiver).collection("Pending Requests").doc(sender);
 
         // delete the doc with key as Uid of sender in receiver's Pending Req Collection
-        const removeSender = await receiverRef.delete();    
+        const removeSender = await receiverRef.delete();
     }
 
     const matchPercent = props.matchPercent;
     return (
         <div>
-            <div className="Sender-Card">
+            {senderDetails !== {} && (
+                <div className="Sender-Card">
                     <div className="Spacer-small"></div>
                     <div className="Card-head">
-                        <Avatar sx={{width:60, height:60}} className="Display-Card-Avatar" src={props.icon} />
+                        <Avatar sx={{ width: 60, height: 60 }} className="Display-Card-Avatar" src={props.icon} />
                         <p>
                             {props.name}
                         </p>
                     </div>
                     <p>
-                        {"Match Percent: "+matchPercent+"%"}
+                        {"Match Percent: " + matchPercent + "%"}
                     </p>
                     <div className="Spacer-large"></div>
                     <div className="button-container">
                         <ButtonGroup size="large">
                             <Button onClick={reqAccept}
-                            sx={{color:"#0984e3", backgroundColor:"black", justifySelf:"flex-end"}} 
-                            variant ="outlined"> <DoneIcon /> </Button>
-                            <Button onClick ={reqDecline} 
-                            sx={{color:"#0984e3", backgroundColor:"black", justifySelf:"flex-end"}} 
-                            variant ="outlined"> <ClearIcon /> </Button>
+                                sx={{ color: "#0984e3", backgroundColor: "black", justifySelf: "flex-end" }}
+                                variant="outlined"> <DoneIcon /> </Button>
+                            <Button onClick={reqDecline}
+                                sx={{ color: "#0984e3", backgroundColor: "black", justifySelf: "flex-end" }}
+                                variant="outlined"> <ClearIcon /> </Button>
                         </ButtonGroup>
                     </div>
                 </div>
-        </div> 
+            )} </div>
     );
-    
-    
+
+
 }
 
 export default SenderCard;
